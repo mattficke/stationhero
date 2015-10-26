c979201b020d7c7389c126d6610f8a9e22c70388
Matt Ficke <matthew.ficke@gmail.com>
2015-10-26 16:45:12 -0400

    add refresh feature

--- js/controllers/stations.js
+++ js/controllers/stations.js
@@ -3,32 +3,37 @@
 
   stationControllers.controller("stationsController", ['$scope', 'Station', 'Status', function($scope, Station, Status){
     var self = this;
-    this.stations = Status.query();
-    var stationIds = Station.query();
-    this.stations.$promise.then(function(result){
-      $scope.stations = result;
-      for(var i=0; i<$scope.stations.length; i++){
-        var station = $scope.stations[i]
 
-        // match the cabi_id to the database id
-        var stationId = stationIds.filter(function(station){
-          return station.cabi_id == $scope.stations[i].id
-        })
+    this.drawMarkers = function(){
+      this.station = undefined;
+      $scope.markers = {}
+      self.stations = Status.query();
+      var stationIds = Station.query();
+      self.stations.$promise.then(function(result){
+        $scope.stations = result;
+        for(var i=0; i<$scope.stations.length; i++){
+          var station = $scope.stations[i]
 
-        $scope.markers["" + station.name + "" ] = {
-          group: 'center',
-          lat: parseFloat(station.lat),
-          lng: parseFloat(station.long),
-          message: '<h3>' + station.name + '</h3>' +
-                    '<div>Available Bikes: ' + station.nbBikes + '</div>' +
-                    '<div>Empty Bike Docks: ' + station.nbEmptyDocks + '</div>' +
-                    '<button ng-click="stationsCtrl.showStation('+ stationId[0].id + ')">show prediction</button>',
-          getMessageScope: function(){
-            return $scope
+          // match the cabi_id to the database id
+          var stationId = stationIds.filter(function(station){
+            return station.cabi_id == $scope.stations[i].id
+          })
+
+          $scope.markers["" + station.name + "" ] = {
+            group: 'center',
+            lat: parseFloat(station.lat),
+            lng: parseFloat(station.long),
+            message: '<h3>' + station.name + '</h3>' +
+                      '<div>Available Bikes: ' + station.nbBikes + '</div>' +
+                      '<div>Empty Bike Docks: ' + station.nbEmptyDocks + '</div>' +
+                      '<button ng-click="stationsCtrl.showStation('+ stationId[0].id + ')">show prediction</button>',
+            getMessageScope: function(){
+              return $scope
+            }
           }
         }
-      }
-    })
+      })
+    };
 
     this.showStation = function(id){
       this.station = Station.get({id: id})
@@ -53,6 +58,8 @@
         }
       }
     });
+
+    this.drawMarkers();
 
   }]);
 
